2c70bcfae573f0205b68bcbb45468931be8d49eb
Daniel <danmobbs.101@gmail.com>
2021-10-02 07:59:55 +0100

    learning about super globals

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php include 'serverinfo.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>system Info</title>
+
+    <link ref="stylesheet" href="css/bootstrap.min.css">    
+    
+</head>
+<body>
+
+    <div class="container">
+        <h1>Server & File Info</h1>
+
+        <?php if($server): ?>
+
+            <ul class="list-group">
+                <?php foreach($server as $key => $value): ?>
+                    <li class="list-group-list">
+                        <strong><?php echo $key; ?>: </strong>
+                        <?php echo $value; ?>
+                    </li>
+                 <?php endforeach; ?>   
+            </ul>
+
+        <?php endif; ?>   
+        
+        <h1>Client Info</h1>
+
+        <?php if($client): ?>
+
+            <ul class="list-group">
+                <?php foreach($client as $key => $value): ?>
+                    <li class="list-group-list">
+                        <strong><?php echo $key; ?>: </strong>
+                        <?php echo $value; ?>
+                    </li>
+                 <?php endforeach; ?>   
+            </ul>  
+            
+        <?php endif; ?>         
+    </div>
+    
+</body>
+</html>
